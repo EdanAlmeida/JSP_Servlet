b153cc61040066917ddc7d2f4f719e0491635b5a
Edan <edan.almeida.nextage@gmail.com>
2023-07-28 14:36:43 -0300

    Feature logout - ajustes ServletLogin e front

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -23,7 +23,16 @@ public class ServletLogin extends HttpServlet {
 	// Recebe os dados pela url em paramêtros
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		String acao = request.getParameter("acao");
+		
+		if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+			redirecionar.forward(request, response);
+		} else {
+			doPost(request, response);
+		}
+		
 	}
 
 	// Recebe os dados enviados por um formulário
